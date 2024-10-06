c61911dad44e76fb68a6b40192ac024d2475e694
Arcy91 <arday91@gmail.com>
2024-10-06 17:02:04 +0200

    React form pom

--- React form/main.py
+++ React form/main.py
@@ -18,4 +18,25 @@ assert ERRORMESSAGES["name"] == alert.text
 
 alert.accept()
 
-#TC 2
+# TC 2
+page.input_firstname().send_keys("Zoltán")
+page.input_lastname().send_keys("Nagy")
+
+page.submit_button().click()
+alert = page.alert_popup()
+
+assert ERRORMESSAGES["phone"] == alert.text
+
+alert.accept()
+
+# TC 3
+page.input_firstname().clear()
+page.input_firstname().send_keys("Zoltán")
+page.input_lastname().send_keys("Nagy")
+page.input_phone().send_keys("2222222222")
+page.submit_button().click()
+alert = page.alert_popup()
+
+assert ERRORMESSAGES["mail"] == alert.text
+
+alert.accept()
